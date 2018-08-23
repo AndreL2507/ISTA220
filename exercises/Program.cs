7d3eb3af5d55b09ccc8c5c654f75a9f2aec57c8c
AndreL2507 <42379575+AndreL2507@users.noreply.github.com>
2018-08-23 08:30:47 -0400

    Add files via upload

---
+++ exercises/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the Grade");
+            double grade = Convert.ToDouble(Console.ReadLine());
+            char LetterGrade = letterGrade(grade);
+            Console.WriteLine($"The Grade is {LetterGrade}");
+
+           
+         
+           
+            
+
+           
+            
+        }
+
+        private static char letterGrade(double grade)
+        {
+            char letter = 'f';
+                
+
+            //throw new NotImplementedException();
+             if (grade >= 90) 
+            {
+                letter = 'A';
+            }
+             else if(grade >= 80 && grade < 90)
+            {
+                letter = 'B';
+            }
+             else if (grade >= 70 && grade < 80)
+            {
+                letter = 'C';
+            }
+             else if (grade >= 60 && grade < 70)
+            {
+                letter = 'D';
+            
+            }
+             else if (grade > 0 && grade < 60)
+            {
+                letter = 'F';
+            }
+
+            return letter;
+        }
+    }
+}
